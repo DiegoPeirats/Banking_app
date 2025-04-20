611cdd266f4be0a1f66b4e08e0b4133bea9d6bcf
L0KODOM <diedom4business@gmail.com>
2025-04-20 23:25:40 +0200

    corrected endpoints

--- user-service/src/main/java/com/diego_peirats/infrastructure/controller/UserController.java
+++ user-service/src/main/java/com/diego_peirats/infrastructure/controller/UserController.java
@@ -2,7 +2,6 @@ package com.diego_peirats.infrastructure.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
