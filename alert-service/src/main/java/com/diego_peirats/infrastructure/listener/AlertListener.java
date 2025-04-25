46690c34f4790fe37411361da2c0e3829835365f
L0KODOM <diedom4business@gmail.com>
2025-04-25 13:47:33 +0200

    modified topic

--- alert-service/src/main/java/com/diego_peirats/infrastructure/listener/AlertListener.java
+++ alert-service/src/main/java/com/diego_peirats/infrastructure/listener/AlertListener.java
@@ -1,12 +1,10 @@
 package com.diego_peirats.infrastructure.listener;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import com.diego_peirats.domain.entity.Alert;
-import com.diego_peirats.infrastructure.repository.AlertRepository;
+import com.diego_peirats.application.service.AlertServiceImpl;
 
 import alert.AlertEvent;
 import lombok.extern.slf4j.Slf4j;
@@ -16,19 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 public class AlertListener {
 	
 	@Autowired
-	private AlertRepository repository;
-	
-	@Autowired
-	private ModelMapper modelMapper;
+	private AlertServiceImpl service;
 	
 	@KafkaListener(
 			topics = "${spring.kafka.topic.name}",
 			groupId = "${spring.kafka.consumer.group-id}"
 		)
 	public void consume(AlertEvent event) {
-		log.info(String.format("Transaction event recieved in stock service => %s", event.toString()));
-		Alert alert = modelMapper.map(event.getAlert(), Alert.class);
-		repository.save(alert);
+		log.info(String.format("Alert event recieved in stock service => %s", event.toString()));
+		service.sendAlert(event);
 	}
 
 }
